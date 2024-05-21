ff59b45093c51222ad6cf67645bc409aa3c25b80
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-05-22 01:31:13 +0300

    added test for EventsService updateEvent in it

---
+++ src/events/services/events.service.spec.ts
@@ -0,0 +1,47 @@
+import { Repository } from 'typeorm';
+import { EventsService } from './events.service';
+import { Event } from '../entities/event.entity';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+describe('EventsService', () => {
+  let service: EventsService;
+  let repository: Repository<Event>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        EventsService,
+        {
+          provide: getRepositoryToken(Event),
+          useValue: {
+            save: jest.fn(),
+            createQueryBuilder: jest.fn(),
+            delete: jest.fn(),
+            where: jest.fn(),
+            execute: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<EventsService>(EventsService);
+    repository = module.get<Repository<Event>>(getRepositoryToken(Event));
+  });
+
+  describe('updateEvent', () => {
+    it('should update the event', async () => {
+      const repoSpy = jest
+        .spyOn(repository, 'save')
+        .mockResolvedValue({ id: 1 } as Event);
+
+      expect(
+        service.updateEvent(new Event({ id: 1 }), {
+          name: 'New name',
+        }),
+      ).resolves.toEqual({ id: 1 });
+
+      expect(repoSpy).toHaveBeenCalledWith({ id: 1, name: 'New name' });
+    });
+  });
+});
