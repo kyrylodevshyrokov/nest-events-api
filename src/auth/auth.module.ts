f9005c91185a3cf2429c2e72b6c61a12a6a17a1d
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-05-21 15:09:01 +0300

    added jwt token on registration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.AUTH_SECRET,
+        signOptions: {
+          expiresIn: '60m',
+        },
+      }),
+    }),
+  ],
+  providers: [LocalStrategy, AuthService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,6 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {}
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
+    super();
+  }
+
+  public async validate(username: string, password: string): Promise<any> {
+    const user = await this.userRepository.findOneBy({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    if (password !== user.password) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
