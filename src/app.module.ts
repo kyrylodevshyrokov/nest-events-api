15b1e182023b1d2fa253b2a1f14a53f75907fc62
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-05-22 13:06:01 +0300

    added e2e testing for events
    + uploaded graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { ConfigModule } from '@nestjs/config';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
 import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -16,10 +18,16 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       load: [ormConfig],
       expandVariables: true,
+      envFilePath: `${process.env.NODE_ENV}.env`,
     }),
     TypeOrmModule.forRootAsync({
       useFactory:
         process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
     }),
     AuthModule,
     EventsModule,
